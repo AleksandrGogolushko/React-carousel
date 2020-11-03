176d7cb74c385906fa4ec1b3cf9d7ae005198fde
Aleksander <gogolushko@outlook.com>
2020-11-03 15:15:55 +0300

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import style from "./App.css"
+
+ export const App = () =>{
+    return (
+        <div>
+           <h1 className={style.title}>Hello Reaact</h1>
+        </div> 
+    )
+}
